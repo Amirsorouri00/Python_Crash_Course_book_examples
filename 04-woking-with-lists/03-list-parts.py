1935d3d1edc6e3390680a1b455b3a214c0563c41
Amir Hossein Sorouri <amirsorouri26@gmail.com>
2020-06-16 00:37:05 +0430

    03 example modifed and completed.

--- 04-woking-with-lists/03-list-parts.py
+++ 04-woking-with-lists/03-list-parts.py
@@ -19,3 +19,9 @@ print(my_foods)
 
 print("\nMy friend's favorite foods are: ")
 print(friend_foods)
+
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+
+print(my_foods)
+print(friend_foods)

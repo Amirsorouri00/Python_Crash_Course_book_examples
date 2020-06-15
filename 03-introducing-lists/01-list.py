65d8ab8713e5ea0234a220a62624caccd115e4e6
Amir Hossein Sorouri <amirsorouri26@gmail.com>
2020-06-15 06:51:12 +0430

    chapter 3 examples added and tested.

---
+++ 03-introducing-lists/01-list.py
@@ -0,0 +1,9 @@
+# In python square brackets ([]) indicate a list.
+bicycles = ['trek', 'cannodale', 'redline', 'specialized']
+print(bicycles)
+
+# Access elements in a list
+print(bicycles[0])
+
+# You can access string methods on any string elements in a list.
+print(bicycles[0].title())
---
+++ 03-introducing-lists/03-organize-list.py
@@ -0,0 +1,16 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
+
+# reverse sort
+cars.sort(reverse=True)
+print(cars)
+
+# sorted function which lets you display list in a particular order but doesn't affect list itself
+print(cars)
+print(sorted(cars))
+print(cars)
+
+# reverse the orders of elements in a list
+cars.reverse()
+print(cars)
